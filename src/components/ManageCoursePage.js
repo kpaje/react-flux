924d8ae5aa954e19f2cc7a31820c0397cac6cf6b
kpaje <kelvinmembership@gmail.com>
2019-08-01 14:33:28 -0700

    created forms with controlled components. created reusable TextInput component. Able to save a course via api call. redirected after saving with route. Toast notification after saving. Form validation. Able to read url slug, populate form, edit, then save with api call.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { Toast, toast } from "react-toastify";
 
 function ManageCoursePage(props) {
+	const [errors, setErrors] = useState({});
+	const [course, setCourse] = useState({
+		id: null,
+		slug: "",
+		title: "",
+		authorId: null,
+		category: ""
+	});
+
+	useEffect(() => {
+		const slug = props.match.params.slug; //from path `/courses/:slug`
+		if (slug) {
+			courseApi.getCourseBySlug(slug).then(_course => {
+				setCourse(_course);
+			});
+		}
+	}, [props.match.params.slug]);
+
+	function handleChange({ target }) {
+		const updatedCourse = {
+			...course,
+			[target.name]: target.value
+		};
+		setCourse(updatedCourse);
+	}
+
+	function formIsValid() {
+		const _errors = {};
+
+		if (!course.title) _errors.title = "Title is required";
+		if (!course.title) _errors.authorId = "Author ID is required";
+		if (!course.title) _errors.category = "Category is required";
+
+		setErrors(_errors);
+		//Form is valid if the error obect has no properties
+		return Object.keys(_errors).length === 0;
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		if (!formIsValid()) return;
+		courseApi.saveCourse(course).then(() => {
+			props.history.push("/courses");
+			toast.success("Course Saved!");
+		});
+	}
+
 	return (
 		<>
 			<h2>Manage Course</h2>
-			{props.match.params.slug}
+			<CourseForm
+				errors={errors}
+				course={course}
+				onChange={handleChange}
+				onSubmit={handleSubmit}
+			/>
 		</>
 	);
 }
