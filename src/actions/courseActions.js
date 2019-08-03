d94a0629f3fb143f48720c07ccb156ba950c52e8
kpaje <kelvinmembership@gmail.com>
2019-08-02 20:42:11 -0700

    able to access store instead of straight to api call, able to delete courses

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -4,10 +4,30 @@ import actionTypes from "./actionTypes";
 
 export function saveCourse(course) {
 	return courseApi.saveCourse(course).then(savedCourse => {
-		//dispatcher notifies stores of newly created course
+		// Hey dispatcher, go tell all the stores that a course was just created.
 		dispatcher.dispatch({
-			actionType: actionTypes.CREATE_COURSE,
+			actionType: course.id
+				? actionTypes.UPDATE_COURSE
+				: actionTypes.CREATE_COURSE,
 			course: savedCourse
+		});
+	});
+}
+
+export function loadCourses() {
+	return courseApi.getCourses().then(courses => {
+		dispatcher.dispatch({
+			actionType: actionTypes.LOAD_COURSES,
+			courses: courses
+		});
+	});
+}
+
+export function deleteCourse(id) {
+	return courseApi.deleteCourse(id).then(() => {
+		dispatcher.dispatch({
+			actionType: actionTypes.DELETE_COURSE,
+			id: id
 		});
 	});
 }
--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
 import courseStore from "../stores/courseStore";
-import CourseList from "./Courselist";
+import CourseList from "./CourseList";
 import { Link } from "react-router-dom";
+import { loadCourses } from "../actions/courseActions";
 
 function CoursesPage() {
-	const [courses, setCourses] = useState([]);
+	const [courses, setCourses] = useState(courseStore.getCourses());
 
 	useEffect(() => {
-		setCourses(courseStore.getCourses());
+		courseStore.addChangeListener(onChange);
+		if (courseStore.getCourses.length === 0) loadCourses();
+		return () => courseStore.removeChangeListener(onChange); // cleanup on unmount
 	}, []);
+
+	function onChange() {
+		setCourses(courseStore.getCourses());
+	}
 
 	return (
 		<>
